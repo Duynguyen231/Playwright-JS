e1d63cf0466c5e5f44783a4e6437b48281364654
Duy Nguyen <nguyenthanhduy230198@gmail.com>
2024-05-22 21:54:42 +0700

    Update commeit

---
+++ Playwright/tests/Login/login.spec.js
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('Basic Test 12', async ({ page }) => {
+  await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+  await expect(page.getByRole('button')).toHaveText('Sign in', { timeout: 10000 });
+  await expect(page.getByRole('button')).not.toContainText('some text');
+})
